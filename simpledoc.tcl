6b4a757c26b0447a60bd633e47c7af8ea6450f8b
Graeme Pietersz <graeme@pietersz.net>
2017-09-14 14:34:36 +0100

    Updated copyright

--- simpledoc.tcl
+++ simpledoc.tcl
@@ -323,7 +323,6 @@ array set HTML {
     /var	</span>
 }
 
-#Jeff Hobb's tcl2html. Adapted from original script that was BSD licensed.
 proc tcl2html {data} {
     global HTML COMMANDS
 
@@ -399,5 +398,5 @@ if [file isdirectory $infile] {
 }
 
 #==Copyright
-#Copyright (c) Graeme Pietersz. Licensed under the GPL version 2 or later. Incorporates BSD licensed code by Jeff Hobbs.
+#Copyright (c) 2008, 2017 Graeme Pietersz. Licensed under the GPL version 2 or later. Incorporates BSD licensed code by Jeff Hobbs.
 #This program is free software: you can redistribute it and/or modify it under the terms of the GNU General Public License as published by the Free Software Foundation, either version 2 of the License, or (at your option) any later version. This program is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License for more details. You should have received a copy of the GNU General Public License along with this program.  If not, see http://www.gnu.org/licenses/
